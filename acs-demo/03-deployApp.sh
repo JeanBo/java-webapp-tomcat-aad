ddfb96b23229aafd98cebcb060cba67cea03191a
chris vugrinec <chris@Air-chris.lan>
2016-06-02 10:58:01 +0200

    bugfile 03-deployApp.sh

--- acs-demo/03-deployApp.sh
+++ acs-demo/03-deployApp.sh
@@ -27,9 +27,9 @@ dcos marathon app add $jsonappfile
 
 # config loadbalancer for app
 lbName=$(azure group show $resourcegroupname | grep -i lb | grep agent | grep Name | sed 's/^.*[:][ ]//')
-appName=`echo jsonappfile | sed 's/.json//'`
+appName=`echo $jsonappfile | sed 's/.json//'`
 azure network lb rule create -g $resourcegroupname -l $lbName -n $appName -p tcp -f $serviceport -b $serviceport
 
 # config network security
 nsgName=$(azure network nsg list -g $resourcegroupname | grep agent | grep public | awk '{print $2}')
-azure network nsg rule create -g $resourcegroupname -a $nsgName -n $appName -rule -c Allow -p Tcp -r Inbound -y $rulenr -f Internet -u $serviceport 
+azure network nsg rule create -g $resourcegroupname -a $nsgName -n $appName-rule -c Allow -p Tcp -r Inbound -y $rulenr -f Internet -u $serviceport 
