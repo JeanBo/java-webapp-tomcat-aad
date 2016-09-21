2af702638f243ce1045f032c93bcd443b892de0c
chris vugrinec <chris@Air-chris.lan>
2016-09-21 23:07:35 +0200

    stable version

--- arduino_onoffswitchWifiHttpWeb/onoffswitch.ino
+++ arduino_onoffswitchWifiHttpWeb/onoffswitch.ino
@@ -4,13 +4,16 @@
 #include <EEPROM.h>
 #include <SPI.h>
 #include <WiFi.h>
+//#include <SD.h>
+#include <TimeLib.h>
 
 
-char ssid[] = "SSID goes here";      //  your network SSID (name)
-char pass[] = "Passwd goes here";   // your network password
+char ssid[] = "datalinks-wifi";      //  your network SSID (name)
+char pass[] = "PuBoLot2015!";   // your network password
 IPAddress ip(192, 168, 1, 104);
-IPAddress googleIp(172,217,17,78);
-IPAddress localhost(127, 0, 0, 1);
+//IPAddress googleIp(172,217,17,78);
+//IPAddress localhost(127, 0, 0, 1);
+//File logFile;
 
 int LAMP1_PIN = 8;
 int RESET_PIN = 5;
@@ -19,12 +22,9 @@ int status = WL_IDLE_STATUS;
 int LOOPLIMIT = 50;
 int counter = 0;
 int loopCounter = 0;
-boolean lastConnected = false;
-unsigned long lastConnectionTime = 0;
-const unsigned long postingInterval = 10*1000;
+
 
 WiFiServer server(80);
-WiFiClient wclient;
 
 
 WiFiServer startWifiServer(){
@@ -58,17 +58,26 @@ void setup() {
   Serial.print(F("Firmware version: "));
   Serial.println(WiFi.firmwareVersion());
 
-  
+
+  //  Setup of pins  
   digitalWrite(RESET_PIN, HIGH);
   delay(200);
 
   pinMode(RESET_PIN, OUTPUT);
   pinMode(LAMP1_PIN, OUTPUT);
 
-
+  /*  SD Card init
+  if (!SD.begin(4)) {
+    Serial.println("initialization of SD Card failed!");
+    return;
+  }
+*/
+  //  Wifishield
   startWifiServer();
   printWifiStatus(); 
   delay(200);
+
+  //  Setup of Time
 
 }
 
@@ -76,9 +85,14 @@ void setup() {
 void loop() {
 
   WiFiClient client = server.available();   // listen for incoming clients
-  loopCounter++;
+  /*loopCounter++;
 
   Serial.println(loopCounter);  
+
+  time_t t = processSyncMessage();
+  if (t != 0) {
+    setTime(t);
+  }
   
   if(loopCounter>=LOOPLIMIT){
     loopCounter=0;
@@ -86,12 +100,25 @@ void loop() {
       status = WiFi.begin(ssid, pass);
       if ( status != WL_CONNECTED) {
         Serial.println("Server not available..rebooting");
+        logFile = SD.open("logs.txt", FILE_WRITE);
+        logFile.println("System reboot at: ");
+        logFile.print(day());
+        logFile.print("-");
+        logFile.print(month());
+        logFile.print(" [ ");
+        logFile.print(hour());
+        logFile.print(":");
+        logFile.print(minute());
+        logFile.print(":");
+        logFile.print(second());          
+        logFile.print(" ]");
+        logFile.close();
         wdReset();          
       }
     }
   }
   delay(1000);
-  
+  */
   if (!client) {
     return;
   }else{
@@ -145,6 +172,8 @@ void loop() {
 
 }
 
+
+
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -167,22 +196,46 @@ void printWifiStatus() {
 
 void wdReset(){
 //  int countdownMS = Watchdog.enable(4000);
+  Serial.print("Get ready, the watchdog will reset ");
+  // Clearing memory
+  for(int i=0; i< EEPROM.length(); i++){
+    EEPROM.write(i,0);
+  }
   Watchdog.reset();
-  Serial.print("Get ready, the watchdog will reset in ");
+  //Serial.print("Get ready, the watchdog will reset in ");
   //Serial.print(countdownMS, DEC);
-  Serial.println(" milliseconds!");
+  //Serial.println(" milliseconds!");
 }
 
+/*
 void softReset(){
   // Clearing memory
   for(int i=0; i< EEPROM.length(); i++){
     EEPROM.write(i,0);
   }
   setup();
-//  asm volatile ("  jmp 0");
+  asm volatile ("  jmp 0");
 }
+*/
 
 void hardReset(){
   digitalWrite(RESET_PIN,LOW);
 }
+
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+
+unsigned long processSyncMessage() {
+  unsigned long pctime = 0L;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     return pctime;
+     if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+       pctime = 0L; // return 0 to indicate that the time is not valid
+     }
+  }
+  return pctime;
+}
+
 
