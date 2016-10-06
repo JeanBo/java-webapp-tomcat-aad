57a56fdc0edfe3700ff9853b8e4430eabd3fa8e6
chris vugrinec <chris@macbook-air-chris.local>
2016-10-06 15:29:43 +0200

    added provisioning

--- adal-auth-ubuntu/aad-login/install.sh
+++ adal-auth-ubuntu/aad-login/install.sh
@@ -44,7 +44,10 @@ apt-get install npm
 npm install
 apt-get install nodejs
 apt-get --purge remove node
-ln -s /usr/bin/nodejs /usr/bin/node
+if [ ! -f /etc/pam.d/common-auth ] 
+then
+  ln -s /usr/bin/nodejs /usr/bin/node
+fi
 
 sed -i 's/aadDirName/'$aadDirName'/' aad-login.js
 sed -i 's/aadClientID/'$aadClientID'/' aad-login.js
