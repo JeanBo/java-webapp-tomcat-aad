5f9f66e4efc78f82edb04c91d8a595bc19b8c9db
chris vugrinec <chris@macbook-air-chris.local>
2016-11-24 12:48:20 +0100

    make prefix unique

--- acs-kubernetes/acs-template/deploy.sh
+++ acs-kubernetes/acs-template/deploy.sh
@@ -1,15 +1,34 @@
+#!//bin/bash
+
+if [[ ! -f ~/.ssh/id_rsa.pub ]]
+then
+  echo "Could not find id_rsa.pub file....please create it with ssh-keygen -t rsa"
+  exit 1
+else
+  echo "have you pasted your SSH KEY in the azuredeploy.parameters.json file? , type Y for yes"
+  read ask1
+  if [[ $ask1 != "Y" ]]
+  then
+    echo "exiting..."
+    exit 0
+  fi
+fi
+
 #azure login
 azure account show
 #echo "enter subscription id"
 #read subid
 #azure account set --name $subid
 
+#echo "enter resourcegroup name"
+#read rgroupname
+dname=`uuidgen`
 
-echo "enter resourcegroup name"
-read rgroupname
-echo "enter deployment name"
-read dname
 
+sed -in 's/ENDPOINTPREFIX/'$dname'/' azuredeploy.parameters.json
+
+
+exit 1
 azure group create \
     --name=$rgroupname \
     --location="east us"
