c4d6bcba5d2b323d0ea0b71f55625ba77eaf24ea
chris vugrinec <chris@192.168.2.116>
2016-12-02 19:38:37 +0100

    bfix

--- gis-abatchjob-redis/get.sh
+++ gis-abatchjob-redis/get.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export AZURE_STORAGE_CONNECTION_STRING=$(cat config/config.txt | grep azure_connection_string | sed 's/azure_connection_string=//')
+export AZURE_STORAGE_CONNECTION_STRING=$(cat /opt/scripts/config/config.txt | grep azure_connection_string | sed 's/azure_connection_string=//')
 filetoget=$1
-wget $filetoget -O temp.zip
+wget $filetoget -O /opt/scripts/temp.zip
 xxx=$(unzip /opt/scripts/temp.zip)
 unzippedfile=$(echo $xxx | awk '{print $4}')
 echo "unzipped file: "$unzippedfile
-azure storage blob upload /opt/scripts/$unzippedfile ahndata
+azure storage blob upload /$unzippedfile ahndata
 rm -f /opt/scripts/temp.zip
-rm -f /opt/scripts/$unzippedfile
+rm -f /$unzippedfile
